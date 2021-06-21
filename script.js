1fdd98697d585f86b70706799ac351fc6f9bb589
HaydenLangill <84516925+Hayden-Langill@users.noreply.github.com>
2021-06-21 15:02:12 +0000

    final

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ const image1 = document.getElementById('image1')
 image1.style.visibility = 'hidden'
 const image2 = document.getElementById('image2')
 image2.style.visibility = 'hidden'
+const image3 = document.getElementById('image3')
+image3.style.visibility = 'hidden'
 
 clicker.addEventListener('click', add)
 
 function add () {
-  if (timesClicked >= 50 && timesClicked < 300) { /* once you hit 50 you click for x2 the   amount */
+  if (timesClicked >= 50 && timesClicked < 300) { // once you hit 50 you click for x2 the   amount */
     timesClicked++
   } else if (timesClicked >= 300 && timesClicked < 1000) { // once you hit 300 you click for x4 the amount
     timesClicked++
@@ -22,7 +24,23 @@ function add () {
     timesClicked++
     timesClicked++
     timesClicked++
-  } else if (timesClicked >= 5000) {
+  } else if (timesClicked >= 5000 && timesClicked <= 7000) {
+    timesClicked++
+    timesClicked++
+    timesClicked++
+    timesClicked++
+    timesClicked++
+    timesClicked++
+    timesClicked++
+    timesClicked++
+  } else if (timesClicked >= 7000) {
+    timesClicked++
+    timesClicked++
+    timesClicked++
+    timesClicked++
+    timesClicked++
+    timesClicked++
+    timesClicked++
     timesClicked++
     timesClicked++
     timesClicked++
@@ -35,7 +53,7 @@ function add () {
   if (code.value === 'Summer' && timesClicked < 500) {
     timesClicked = 500
   } else if (code.value === 'Finish' && timesClicked < 10000) {
-    timesClicked = 9900
+    timesClicked = 4800
   }
   // base start without boost
   timesClicked++
@@ -55,8 +73,10 @@ function add () {
     goal.innerHTML = 5000 - timesClicked
     image2.style.visibility = 'visible'
     document.getElementById('body').style.backgroundColor = 'blue'
-  } else if (timesClicked >= 5000 && timesClicked <= 10000) {
-    alert('Your half way there!')
+  } else if (timesClicked >= 5000 && timesClicked <= 7000) {
+    image3.style.visibility = 'visible'
+  } else if (timesClicked >= 7000 && timesClicked <= 10000) {
+    document.getElementById('text').innerHTML = 'Last objective'
   }
   if (timesClicked === 10000) {
     const audio = new Audio('Legends.webm') // If player gets 10,000 clicks it plays this song
